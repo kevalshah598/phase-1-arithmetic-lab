26d891ac381dc593400bfc69e9c0105cac91ce0d
Keval Shah <kevalshah598@gmail.com>
2023-07-13 10:15:01 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2
+const num2 = 31
+const multiply = (num1 * num2)
 
-// Write your code here
+const random = Math.ceil(Math.random())
+
+const num3 = 9
+const num4 = 5
+const mod = (num3 % num4)
+
+const max = Math.max(1,2,3,20)
